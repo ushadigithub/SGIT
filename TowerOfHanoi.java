74bd47aed676c319ce33b4d4f68e14477106af45
ushadigithub <ushadinakaram6@gmail.com>
2023-08-01 14:32:15 +0530

    commit

---
+++ TowerOfHanoi.java
@@ -0,0 +1,17 @@
+public class TowerOfHanoi {
+    public static void main(String[] args) {
+        int n = 3;
+        shiftDisk(n,'A','B','C');
+    }
+    private static void shiftDisk(int n, char TA, char TB, char TC) {
+        if (n==1){
+            System.out.println("Disk 1 is shifted from "+TA+ "to "+TC);
+        }else {
+            shiftDisk(n-1,TA,TC,TB);
+            System.out.println("Disk " +n+"from "+TA+ " is shifted to "+TC);
+            shiftDisk(n-1,TB,TA,TC);
+
+        }
+    }
+
+}
