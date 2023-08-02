5440b09309ac9297d521c9e3b4542e33f3ff9efe
ushadigithub <ushadinakaram6@gmail.com>
2023-08-02 16:46:01 +0530

    2/8/23

---
+++ CountCharacters.java
@@ -0,0 +1,28 @@
+
+public class CountCharacters {
+    public static void main(String[] args) {
+        int vowels = 0, consonants = 0,whitespaces=0;
+
+        String str1 = "Every One Should Learn";
+        str1 = str1.toLowerCase();
+        for (int i = 0; i < str1.length(); i++) {
+            char ch=str1.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowels++;
+            } else if (ch >= 'a' && ch <= 'z') {
+                consonants++;
+            } else if (Character.isWhitespace(ch)) {
+                whitespaces++;
+            }
+        }
+        System.out.println("Number of vowels:"+vowels);
+        System.out.println("Number of consonants:"+consonants);
+        System.out.println("Number of whitespaces:"+whitespaces);
+    }
+}
+
+
+
+
+
+
