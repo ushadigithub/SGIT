11395ee8ecd41eaeaac41adec893ac2f2175b709
ushadigithub <ushadinakaram6@gmail.com>
2023-08-03 12:41:19 +0530

    3/8/23

---
+++ StringConcat.java
@@ -0,0 +1,8 @@
+public class StringConcat {
+    public static void main(String[] args) {
+        String s1="sharp";
+        String s2="gurus";
+        String result=s1.concat(s2);
+        System.out.println(result);
+    }
+}
