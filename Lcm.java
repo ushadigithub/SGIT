f0d874c8d72e526f40c3dc31786305d1aa01c163
ushadigithub <ushadinakaram6@gmail.com>
2023-08-09 14:50:40 +0530

    9/8/23

--- Lcm.java
+++ Lcm.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class lcm {
+public class Lcm {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the first number:");
---
+++ SubString.java
@@ -0,0 +1,10 @@
+public class SubString {
+    public static void main(String[] args) {
+        String str="usha";
+        for (int i=0;i<str.length();i++){
+            for (int j=i+1;j<=str.length();j++){
+                System.out.println(str.substring(i,j));
+            }
+        }
+    }
+}
