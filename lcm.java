57245a96888002ca17448171ab369cc50477e743
ushadigithub <ushadinakaram6@gmail.com>
2023-08-09 10:30:51 +0530

    9/8/23

---
+++ lcm.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class lcm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number:");
+        int num2 = sc.nextInt();
+        int lcm = Math.max(num1, num2);
+        while (true) {
+            if (lcm % num1 == 0 && lcm % num2 == 0) {
+                System.out.println("lcm of entered numbers:" + lcm);
+                break;
+            }
+            ++lcm;
+        }
+    }
+}
