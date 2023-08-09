f6d3f35386e47520c7dd345a0bdd868bf9cbbee5
ushadigithub <ushadinakaram6@gmail.com>
2023-08-09 13:20:46 +0530

    9/8/23

---
+++ SortString.java
@@ -0,0 +1,17 @@
+public class SortString {
+    public static void main(String[] args) {
+        String str="sunny";
+        char[] arr =str.toCharArray();
+        char temp;
+        for (int i=0;i< arr.length;i++){
+            for (int j=i+1;j< arr.length;j++){
+                if (arr[i]>arr[j]){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.println(new String(arr));
+    }
+}
