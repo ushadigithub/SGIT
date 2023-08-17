f8a8b59f21cc565a5b45e834cd9d1babd2dfa835
ushadigithub <ushadinakaram6@gmail.com>
2023-08-17 18:09:55 +0530

    17/8/23

--- CommandLineArguments.java
+++ CommandLineArguments.java
@@ -1,7 +1,7 @@
 public class CommandLineArguments {
     public static void main(String[] args) {
         //check if any command line arguments are provided
-        if (args.length==3){
+        if (args.length==0){
             System.out.println("No command-lene arguments are provided:");
         }else {
             System.out.println("command line-arguments are provided:");
---
+++ CosineSeries.java
@@ -0,0 +1,21 @@
+public class CosineSeries {
+    public static void main(String[] args) {
+        int terms = 10; // Number of terms in the series
+        double x = Math.PI / 3; // Angle in radians (example: pi/3)
+        double sum = 1.0; // First term of the series (cos(0) = 1)
+        double power = 1.0; // Initialize power of x
+        int sign = -1; // Initialize the sign (negative for the first term)
+        {
+            System.out.println("Cosine Series:");
+        }
+        for (int i = 1; i < terms; i++) {
+            power *= x * x; // Calculate the next power of x (x^n)
+            int factorial = 2 * i; // Calculate 2n!
+            sum += (sign * power) / factorial; // Add the next term to the sum
+
+            sign *= -1; // Toggle the sign for the next term
+        }
+
+        System.out.println("Cos(" + x + ") = " + sum);
+    }
+}
